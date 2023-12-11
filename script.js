6a73c2df090812cc282449e2e7d095d62cf10928
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-11 23:19:33 +0100

    Select ordered list and its item and add & remove a class from classList

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const add = (num1, num2) => {
-  const score = num1 + num2;
-  score % 2 === 0 ? sayEven(score) : sayOdd(score);
-}
+const olList = document.querySelector("ol");
+console.log(olList);
 
-const sayEven = (score) => console.log(`Liczba ${score} jest parzysta.`);
-const sayOdd = (score) => console.log(`Liczba ${score} jest nieparzysta.`);
+const firstLi = document.createElement("li");
+firstLi.className = "list-group-item p-0 pb-3";
+firstLi.textContent = "🔹Pierwszy element listy";
 
-add(2, 3);
+olList.appendChild(firstLi);
+console.log(firstLi);
+
+firstLi.classList.add("text-success");
+console.log(firstLi.classList);
+
+firstLi.classList.remove("text-success")
+console.log(firstLi.classList, firstLi.className);
