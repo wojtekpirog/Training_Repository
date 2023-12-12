ce8aaa7a16d9d6489c120244e31b0e64d6893ecc
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-12 22:51:48 +0100

    Create a script to handle an error of incomplete json

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const widthInCm = parseFloat(prompt("Podaj szerokość w centrymetrach:"));
-const heightInCm = parseFloat(prompt("Podaj wysokość w centymetrach:"));
+let json = '{ "age": 30, "name": "Yayami" }';
 
-console.log(`Szerokość: ${widthInCm}`); // widthInCm = NaN
-console.log(typeof widthInCm); // heightInCm = NaN
-console.log(`Wysokość: ${heightInCm}`); 
-console.log(typeof heightInCm); 
-
-if (isNaN(widthInCm) || isNaN(heightInCm)) {
-  console.error("Jedna z podanych wartości (lub obie) nie jest liczbą!");
-} else if (widthInCm <= 0 || heightInCm <= 0) {
-  console.error("Jedna z podanych wartości (lub obie) nie jest liczbą dodatnią!");
-} else {
-  const areaInM2 = (widthInCm * heightInCm) / 10000;
-  console.log(`Powierzchnia wynosi: ${areaInM2} metrów kwadratowych.`);
+try {
+  let user = JSON.parse(json); // <-- nie ma jeszcze błędu
+  if (!user.surname) {
+    throw new SyntaxError("❗Incomplete data: `surname` is missing.❗");
+  }
+  console.log(user.surname); //undefined, to nie jest run-time error
+  console.log("Try code still runs...");
+} catch(err) {
+  alert(`Błąd JSON: ${err.name} - ${err.message}`);
 }
