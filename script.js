9fb2d7043897f6b4fe599a8ab79e433dcfd4acb4
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-13 00:04:02 +0100

    Create a button and add eventListener with a function to check json data

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let json = '{ "age": 30, "name": "Yayami" }';
+const containerDiv = document.querySelector("div.container");
+const button = document.createElement("button");
+button.className = "btn btn-outline-primary p-2 border border-2 fw-bold";
+button.innerText = "Read data";
+button.setAttribute("type", "button");
+containerDiv.appendChild(button);
 
-try {
-  let user = JSON.parse(json); // <-- nie ma jeszcze błędu
-  if (!user.surname) {
-    throw new SyntaxError("❗Incomplete data: `surname` is missing.❗");
+let jsonData = '{"name": "Yayami", "surname": "Omate", "email": "yayami@example.com"}';
+
+const readData = jsonData => {
+  try {
+    let jsData = JSON.parse(jsonData);
+    if (!jsData.email) {
+      throw new SyntaxError("Incomplete data: `email` is missing!");
+    }
+    throwUnexpectedError(); 
+  } catch(err) {
+    if (err instanceof SyntaxError) {
+      console.error(`SyntaxError: ${err.message}`);
+    } else if (err instanceof ReferenceError) {
+      console.error(`ReferenceError: ${err.message}`);
+    } else {
+      throw err;
+    }
   }
-  console.log(user.surname); //undefined, to nie jest run-time error
-  console.log("Try code still runs...");
-} catch(err) {
-  alert(`Błąd JSON: ${err.name} - ${err.message}`);
 }
+
+const logData = () => {
+  try {
+    readData(jsonData);
+  } catch(err) {
+    console.error(`External catch statement caught an error: ${err.name} - ${err.message}`);
+  }
+}
+
+button.addEventListener("click", logData);
