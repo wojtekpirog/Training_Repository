8dac2d59bf58347e3a1b76ab18346eee240a459c
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-16 22:27:42 +0100

    Write a script to send a PUT request with fetch

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const divContainer = document.querySelector("div.container");
-const button = document.createElement("button");
-button.setAttribute("type", "button");
-button.textContent = "Get books";
-button.className = "btn btn-outline-primary border border-2 fs-4 fw-light";
+const btn = document.querySelector("button");
 
-divContainer.appendChild(button);
-
-const fetchData = () => {
-  axios.get(`https://simple-books-api.glitch.me/books`)
-    .then(responseReady => doSomethingWith(responseReady))
-    .catch(error => {
-      console.error(`Error name: ${error.name} | Error message: ${error.message}`);
+function makePutRequest() {
+  fetch("https://reqres.in/api/users/2", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    method: "PUT",
+    // Sending only the fields I want to update:
+    body: JSON.stringify({
+      email: "yayami@example.com",
+      first_name: "Yayami"
     })
-    .finally(() => console.log("Ten kod zawsze się wykona."))   
+  })
+    .then(res => res.json())
+    .then(dataReady => console.log(dataReady))
+    .catch(err => console.error(`Error message: ${err.message}`))
 }
 
-const doSomethingWith = responseReady => {
-  console.log(responseReady);
-}
-
-button.addEventListener('click', fetchData);
+btn.addEventListener("click", () => makePutRequest())
