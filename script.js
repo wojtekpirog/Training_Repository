00985ec64b78f53ac0ad7fddbba1be86178d44aa
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2023-12-22 20:18:31 +0100

    Write a program to print a string of chars backwards

--- script.js
+++ script.js
@@ -1,46 +1,13 @@
-const pageHeading = document.querySelector("h1");
-const btn = document.querySelector("button");
-const containerFluid = document.querySelector("div.container-fluid");
+const string = prompt("Podaj swój nick NA WSPAK");
 
-let listVisible = false;
-
-const toggleList = () => {
-  listVisible ? hideList() : showList();
-  listVisible = !listVisible;
-  console.log(`Lista widoczna: ${listVisible}`);
+function logTxt() {
+  console.log("mówią na mnie:");
 }
 
-const showList = () => {
-  const listContainer = document.createElement("div");
-  const uList = document.createElement("ul"); 
-
-  listContainer.className = "mt-4";
-  containerFluid.append(listContainer);
-
-  uList.className = "list-group list-group-flush";
-  listContainer.append(uList);
-
-  for (let i = 1; i <= 10; i++) {
-    const li = document.createElement("li");
-    li.className = "list-group-item";
-    li.innerText = `🔹Item number: ${i}`;
-    uList.append(li);
-  }
-
-  const lastItem = uList.lastElementChild; 
-  lastItem.innerText = "❗I am the last item❗"
-  lastItem.style.backgroundColor = "blue";
-  lastItem.style.padding = "20px 40px";
-  lastItem.style.fontSize = "20px";
-  lastItem.style.textAlign = "center";
-
-  btn.innerText = "Hide list"
+function reverseString(string) {
+  const reversedString = string.split("").reverse().join("");
+  console.log(reversedString);
 }
 
-const hideList = () => {
-  const listContainer = document.querySelector("div.mt-4");
-  containerFluid.removeChild(listContainer);
-  btn.innerText = "Show list";
-}
-
-btn.addEventListener("click", toggleList);
+setTimeout(logTxt, 1000);
+setTimeout(reverseString, 5000, string);
