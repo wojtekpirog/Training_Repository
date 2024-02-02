7707bd3c8eb9ed75b738f484fe0f568d4ddddfcb
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-02-02 23:40:32 +0100

    Finish project

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Pętla `while` wykonuje się tak długo, jak długo prawdziwy jest warunek w nawiasie
-// Pętli `for` używamy wtedy, gdy wiemy, ile razy (ile czasu) pętla będzie się wykonywać
-// Pętli `while` używamy, gdy chcemy wykonać jakiś kod w pętli, ale nie wiemy, ile razy ta pętla powinna się wykonać
+const paragInFooter = document.querySelector('footer p');
+const nav = document.querySelector(".navbar-collapse");
+const year = new Date().getFullYear();
 
-// Pętla `for...in` przydaje się przy obiektach, natomiast pętla `for...of` - do tablic:
-// Z pętli `for...of` możemy skorzystać, gdy nie mamy zamiaru wykonywać działań na konkretnych elementach (indeksach) tablicy
+document.addEventListener("click", () => {
+  if (nav.classList.contains("show")) {
+    nav.classList.remove("show");
+  }  
+});
+
+paragInFooter.innerHTML = `${year} <span class="orange-color">MXN Studio</span><sup>&copy;</sup>`
