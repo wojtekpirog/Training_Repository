6655c895168e7cead800fc7268263024000c05d2
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-08 22:18:40 +0100

    Create the 'About Us' section and improve scroll padding top on desktop version

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,8 @@ const handleScroll = () => {
   window.addEventListener("scroll", () => {
     if (window.scrollY >= 240) {
       navbar.classList.add("shadow-bg");
-      console.log("Shadow added");
     } else {
       navbar.classList.remove("shadow-bg");
-      console.log("Shadow removed");
     }    
   });
 }
