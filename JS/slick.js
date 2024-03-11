bc2e8d580ecacae2cff84e1d9484f386a1e076e8
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-03-12 00:30:57 +0100

    Include the 'Slick.js' library and transform the 'Team' section into an interactive slideshow

--- JS/slick.js
+++ JS/slick.js
@@ -1,5 +1,25 @@
-// $('.team-carrousel').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
+$('.team-carrousel').slick({
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  accessibility: true,
+  mobileFirst: true,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    }
+  ]
+});
